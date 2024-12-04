d80940a9c07f39cf1d4e48c8b686371f6fa6875e
22035969-Joey <134464241+22035969-Joey@users.noreply.github.com>
2024-12-04 20:48:01 +0800

    Update DrugPredictionApp.py

--- DrugPredictionApp.py
+++ DrugPredictionApp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost as xgb
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
